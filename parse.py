26a5b41f384f0d27c0a76b1403ee3b858608255a
mixedblue <sthakur26@gmail.com>
2019-04-19 15:14:16 -0400

    can read from chase and capital one emails

--- parse.py
+++ parse.py
@@ -1,25 +1,50 @@
 '''
 Parses email for name, date and cost from transaction text from email
-Name is identified by upper case and after 'at', date by '/' and cost by '$'
+Capital One: Name is identified by upper case and after 'at', date by '/' and cost by '$'
+Chase: Name is identified between 'at' and 'has', date by '/' and cost by '($USD)'
 '''
-def parse(s):
+def parse(s, bank):
 	s = s.replace(',', ' ')
 	str = s.split()
-	afterAt = False
 	name = cost = date = ''
-	for w in str:
-		if w == 'at':
-			afterAt = True
-		if w.isupper() and afterAt:
-			name += w + ' '
-		if w[0] == '$':
-			cost = w
-		if len(w) > 2 and w[2] == '/':
-			date = w
-	name = name[:-1]
+	if bank == 'Capital One':
+		afterAt = False
+		for w in str:
+			if w == 'at':
+				afterAt = True
+			if w.isupper() and afterAt:
+				name += w + ' '
+			if w[0] == '$':
+				cost = w[1:]
+			if len(w) > 2 and w[2] == '/':
+				date = w
+		name = name[:-1]
+	elif bank == 'Chase':
+		afterAt = False
+		beforeHas = True
+		costBool = False
+		for w in str:
+			if w == 'at':
+				afterAt = True
+			elif w == 'has':
+				beforeHas = False
+			elif afterAt and beforeHas:
+				name += w + ' '
+			elif w == '($USD)':
+				costBool = True
+			elif costBool:
+				cost = w
+				costBool = False
+			elif len(w) > 2 and w[2] == '/':
+				date = w
+		name = name[:-1]
 	return date, name, cost
 
-emailFile = open('./test-transaction.txt', 'r')
+bank = 'Capital One'
+emailFile = open('./transactions/capitalone.txt', 'r')
+#bank = 'Chase'
+#emailFile = open('./transactions/chase.txt', 'r')
+
 emailText = emailFile.read()
-date, name, cost = parse(emailText)
+date, name, cost = parse(emailText, bank)
 print(f'Spent {cost} at {name} on {date}.')

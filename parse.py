67b196586e4efcc8ba5bb1266fb8b8cf4141884b
mixedblue <sthakur26@gmail.com>
2019-04-19 16:23:11 -0400

    split functions

--- parse.py
+++ parse.py
@@ -1,50 +1,61 @@
 '''
 Parses email for name, date and cost from transaction text from email
-Capital One: Name is identified by upper case and after 'at', date by '/' and cost by '$'
-Chase: Name is identified between 'at' and 'has', date by '/' and cost by '($USD)'
 '''
-def parse(s, bank):
-	s = s.replace(',', ' ')
-	str = s.split()
-	name = cost = date = ''
+#Routes to function based on bank
+def parse(emailText, bank):
 	if bank == 'Capital One':
-		afterAt = False
-		for w in str:
-			if w == 'at':
-				afterAt = True
-			if w.isupper() and afterAt:
-				name += w + ' '
-			if w[0] == '$':
-				cost = w[1:]
-			if len(w) > 2 and w[2] == '/':
-				date = w
-		name = name[:-1]
+		return parseCapitalOne(emailText)
 	elif bank == 'Chase':
-		afterAt = False
-		beforeHas = True
-		costBool = False
-		for w in str:
-			if w == 'at':
-				afterAt = True
-			elif w == 'has':
-				beforeHas = False
-			elif afterAt and beforeHas:
-				name += w + ' '
-			elif w == '($USD)':
-				costBool = True
-			elif costBool:
-				cost = w
-				costBool = False
-			elif len(w) > 2 and w[2] == '/':
-				date = w
-		name = name[:-1]
+		return parseChase(emailText)
+#Capital One: Name is identified by upper case and after 'at', date by '/' and cost by '$'
+def parseCapitalOne(emailText):
+	emailText = emailText.replace(',', ' ')
+	words = emailText.split()
+	name = cost = date = ''
+	afterAt = False
+	for w in words:
+		if w == 'at':
+			afterAt = True
+		if w.isupper() and afterAt:
+			name += w + ' '
+		if w[0] == '$':
+			cost = w[1:]
+		if len(w) > 2 and w[2] == '/':
+			date = w
+	name = name[:-1]
+	return date, name, cost
+#Chase: Name is identified between 'at' and 'has', date by '/' and cost by '($USD)'
+def parseChase(emailText):
+	emailText = emailText.replace(',', ' ')
+	words = emailText.split()
+	name = cost = date = ''
+	afterAt = False
+	beforeHas = True
+	costBool = False
+	for w in words:
+		if w == 'at':
+			afterAt = True
+		elif w == 'has':
+			beforeHas = False
+		elif afterAt and beforeHas:
+			name += w + ' '
+		elif w == '($USD)':
+			costBool = True
+		elif costBool:
+			cost = w
+			costBool = False
+		elif len(w) > 2 and w[2] == '/':
+			date = w
+	name = name[:-1]
 	return date, name, cost
 
+#Choose which bank
 bank = 'Capital One'
 emailFile = open('./transactions/capitalone.txt', 'r')
 #bank = 'Chase'
 #emailFile = open('./transactions/chase.txt', 'r')
 
+#Test code
 emailText = emailFile.read()
 date, name, cost = parse(emailText, bank)
 print(f'Spent {cost} at {name} on {date}.')

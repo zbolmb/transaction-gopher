53506bb4b55447c94217b0cb628ecb2c72bd7cda
mixedblue <sthakur26@gmail.com>
2019-04-23 11:14:52 -0400

    added unit tests

--- parse.py
+++ parse.py
@@ -49,13 +49,26 @@ def parseChase(emailText):
 	name = name[:-1]
 	return date, name, cost
 
-#Choose which bank
-bank = 'Capital One'
-emailFile = open('./transactions/capitalone.txt', 'r')
-#bank = 'Chase'
-#emailFile = open('./transactions/chase.txt', 'r')
+def test_capital_one():
+	bank = 'Capital One'
+	emailFile = open('./transactions/capitalone.txt', 'r')
+	emailText = emailFile.read()
+	date, name, cost = parse(emailText, bank)
+	assert date == '04/17/2019', 'Capital One: date should be 04/17/2019'
+	assert name == 'SUPER CHICKEN', 'Capital One: name should be SUPER CHICKEN'
+	assert cost == '10.38', 'Capital One: cost should be 10.38'
 
-#Test code
-emailText = emailFile.read()
-date, name, cost = parse(emailText, bank)
-print(f'Spent {cost} at {name} on {date}.')
+def test_chase():
+	bank = 'Chase'
+	emailFile = open('./transactions/chase.txt', 'r')
+	emailText = emailFile.read()
+	date, name, cost = parse(emailText, bank)
+	assert date == '04/18/2019', 'Chase: date should be 04/18/2019'
+	assert name == 'Amazon.com', 'Chase: name should be Amazon.com'
+	assert cost == '59.35', 'Chase: cost should be 59.35'
+
+
+if __name__ == "__main__":
+	test_capital_one()
+	test_chase()
+	print("Everything passed")
